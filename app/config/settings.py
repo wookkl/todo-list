08f5a03ec70900139cfbdfcbe3d3250b1cb37893
wookkl <wjddnr315@gmail.com>
2021-01-27 12:53:23 +0900

    Travis-ci configuration

--- app/config/settings.py
+++ app/config/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = os.environ.get("DJANGO_SECRET")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = bool(os.environ.get("DEBUG"))
+DEBUG = True if bool(os.environ.get("DEBUG")) else False
 
 ALLOWED_HOSTS = ["*"]
 
