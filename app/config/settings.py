1b89ca616184d05908f310a1b59788361ca91598
wookkl <wjddnr315@gmail.com>
2021-01-28 15:32:06 +0900

    docker-compose postgredql

--- app/config/settings.py
+++ app/config/settings.py
@@ -94,11 +94,11 @@ if DEBUG:
 else:
     DATABASES = {
         'default': {
-            'ENGINE': 'django.db.backends.postgresql',
-            'HOST': os.environ.get('DB_HOST'),
-            'NAME': os.environ.get('DB_NAME'),
+            'ENGINE': os.environ.get('DB_ENGINE'),
+            'NAME': os.environ.get('DB_DATABASE'),
             'USER': os.environ.get('DB_USER'),
             'PASSWORD': os.environ.get('DB_PASSWORD'),
+            'HOST': os.environ.get('DB_HOST'),
             'PORT': os.environ.get('DB_PORT'),
         }
     }
